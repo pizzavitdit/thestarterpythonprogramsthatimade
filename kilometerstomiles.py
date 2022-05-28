a98d79aeb6f52a778bc607c15af267b85f3ac63c
pizzawithdirt <90923592+pizzavitdit@users.noreply.github.com>
2022-05-28 15:45:49 +0300

    Create kilometerstomiles.py

---
+++ kilometerstomiles.py
@@ -0,0 +1,16 @@
+def repeat():
+    kilometers = int(input("Enter a value: "))
+    miles = kilometers * 0.60934
+
+    if kilometers >= 1:
+         print(miles)
+    else:
+         print("Please enter a valid value.")
+    check = input("Do you want to quit or start again, enter Y to restart or another key to end: ")
+    if check.upper() == "Y": #loop back to the start
+         repeat()
+         exit() #exit the program
+
+repeat()
+
+# Please support me on Patreon: patreon.com/pizzavitditt
